e04578b5cb8da8341198f453f161dde4520ad6c0
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-14 19:08:24 +0500

    add: day 2 learned commenting in python

---
+++ day_2/comments.py
@@ -0,0 +1,6 @@
+
+
+def onlyComments():
+ # This is a single line comment
+ print("This function contains comment come to read it!")
+    
---
+++ day_2/main.py
@@ -0,0 +1,5 @@
+
+from comments import onlyComments
+
+
+onlyComments()
