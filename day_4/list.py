d04cff8d86b2d37b6b14eef2ea9be0f67ae60902
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-16 13:27:05 +0500

    Add: added lists a bit with its methods:

---
+++ day_4/list.py
@@ -0,0 +1,31 @@
+
+def lists():
+    
+    # using list constructor!
+    myList = list(("a",1,"Helo"));
+    
+    print(myList);
+    # using string formatter!
+    print("{0} {1} {2}".format("a",1,"Helo"))
+
+# add some value in list!
+    myList.append("Hello");
+    
+    print(myList)
+    
+    # using insert method!
+    # using append method!
+    # using remove method!
+    # using pop method!
+    # using extend method on list!
+    # remove the value at any index using del keyword!
+    # using clear method!
+    # using sort method!
+    # using reverse method!
+    
+thislist = ["apple", "banana", "cherry"]
+x = [x*2 for x in thislist]
+
+print(x)
+
+lists()
