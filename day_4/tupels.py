dfe34a3ee6e22694b9847af18f6ee1248752cac7
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-16 13:43:49 +0500

    add: tupels in python , and its methods

---
+++ day_4/tupels.py
@@ -0,0 +1,26 @@
+
+
+
+#  lets disucss about tuples!
+
+
+# tupels are kind of fixed as we have in other languages array and they are fixed!!
+# slicing in tules are same as in different languages!
+
+# create an example for tuples in python!
+# tuples are below:
+
+# tuples are immutable!
+# tuples are faster than list!
+# tuples are used in different languages!
+
+# creating a tuple using the tuple constructor!
+# we cannot directly chnage in tupels as they are imputables but we can make them or convert them 
+# into a list and then change them and then convert back into tupels!!
+
+# distructing oin tupels
+
+
+# basic tuple methods are:!
+# count method!
+#  index method!
